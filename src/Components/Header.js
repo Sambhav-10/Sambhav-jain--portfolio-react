7e0e56d649984c3e48182c8e4516885740aec57f
Sambhav-10 <148258778+Sambhav-10@users.noreply.github.com>
2024-05-27 11:13:30 +0530

    Update Header.js
    responsive header

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,19 +1,38 @@
-import React from 'react'
+import React, { useState } from 'react';
 
 const Header = () => {
+  const [mobile, setMobile] = useState(false);
+
+  const toggleMobileMenu = () => {
+    setMobile(!mobile);
+  };
+
   return (
-    <div className='flex justify-center fixed w-full mt-5  '>
-      <div className='flex justify-around  h-16 items-center  bg-opacity-20 w-[90%]  backdrop-filter backdrop-blur-lg border-t-4 rounded-full border-[#77b28c]'>
-        <div className='text-3xl font-bold text-[#77b28c]'>Sambhav jain</div>
-        <div className='flex text-white gap-6 list-none text-xl'>
-            <li className='hover:text-[#77b28c] cursor-pointer'>Home</li>
-            <li className='hover:text-[#77b28c] cursor-pointer'>About</li>
-            <li className='hover:text-[#77b28c] cursor-pointer'>Portfolio</li>
-            <li className='hover:text-[#77b28c] cursor-pointer'>Contact</li>
+    <div className='flex justify-center fixed w-full mt-5'>
+      <div className='flex justify-around h-16 items-center bg-opacity-20 w-[90%] backdrop-filter backdrop-blur-lg border-t-4 rounded-full border-[#77b28c]'>
+        <div className='text-3xl font-bold text-[#77b28c]'>Sambhav Jain</div>
+        <div className={`hidden md:flex text-white gap-6 list-none text-xl`}>
+          <li className='hover:text-[#77b28c] cursor-pointer'>Home</li>
+          <li className='hover:text-[#77b28c] cursor-pointer'>About</li>
+          <li className='hover:text-[#77b28c] cursor-pointer'>Portfolio</li>
+          <li className='hover:text-[#77b28c] cursor-pointer'>Contact</li>
+        </div>
+        <div className="md:hidden">
+          <button onClick={toggleMobileMenu} className="text-white">
+            {mobile ? 'Close' : 'Menu'}
+          </button>
         </div>
       </div>
+      {mobile && (
+        <div className='absolute top-14 right-0 w-1/4  md:hidden  bg-opacity-20  backdrop-filter backdrop-blur-lg  text-white gap-6 list-none text-xl md:w-full'>
+          <li className='hover:text-[#77b28c] cursor-pointer'>Home</li>
+          <li className='hover:text-[#77b28c] cursor-pointer'>About</li>
+          <li className='hover:text-[#77b28c] cursor-pointer'>Portfolio</li>
+          <li className='hover:text-[#77b28c] cursor-pointer'>Contact</li>
+        </div>
+      )}
     </div>
-  )
-}
+  );
+};
 
-export default Header
+export default Header;
